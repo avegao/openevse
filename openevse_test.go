72b5b3b8e18a8d7f39d1eac9bfb57d67593e14db
Álvaro de la Vega Olmedilla <alvarodlvo@gmail.com>
2018-01-16 20:36:05 +0100

    * UPD: Get RTC Time test

--- openevse_test.go
+++ openevse_test.go
@@ -7,11 +7,49 @@ import (
 	"fmt"
 	"net/http"
 	"github.com/avegao/openevse/command"
+	"time"
 )
 
-//func TestGetRtcTime(t *testing.T) {
-//	assert.Equal(t, "a", "a")
-//}
+func TestGetRtcTime(t *testing.T) {
+	defer gock.Disable()
+
+	r := gock.New("http://(.*)").
+		Reply(http.StatusOK).
+		JSON(
+		command.Response{
+			Command:  fmt.Sprintf("$%s", command.GetRtcTime),
+			Response: "$OK 18 0 7 16 24 9^16",
+		})
+
+	assert.NoError(t, r.Error)
+
+	rtcTime, err := GetRtcTime("192.168.1.156")
+
+	assert.NoError(t, err)
+	assert.Equal(t, rtcTime, time.Date(2018, 1, 7, 16, 24, 9, 0, time.UTC))
+
+	now := time.Now()
+	// this is to remove nano seconds
+	now = time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second(), 0, time.UTC)
+	responseTime := fmt.Sprintf("$OK %d %d %d %d %d %d^16", now.Year() - 2000, now.Month() - 1, now.Day(), now.Hour(), now.Minute(), now.Second())
+
+	r = gock.New("http://(.*)").
+		Reply(http.StatusOK).
+		JSON(
+		command.Response{
+			Command:  fmt.Sprintf("$%s", command.GetRtcTime),
+			Response: responseTime,
+		})
+
+	assert.NoError(t, r.Error)
+
+	rtcTime, err = GetRtcTime("192.168.1.156")
+
+	assert.NoError(t, err)
+	assert.Equal(t, rtcTime, now)
+
+	assert.True(t, gock.IsDone())
+}
 
 func TestGetVersion(t *testing.T) {
 	const firmwareVersionExpected = "4.8.0"
