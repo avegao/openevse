e7bae89685c0793ba07d6c197b953781bf38c769
Álvaro de la Vega Olmedilla <alvarodlvo@gmail.com>
2018-01-28 21:26:19 +0100

    * ADD: GetOverTemperatureThresholds

---
+++ command/get/over_temperature_thresholds/get_over_temperature_thresholds.go
@@ -0,0 +1,63 @@
+package getOverTemperatureThresholds
+
+import (
+	"strings"
+	"errors"
+	"github.com/avegao/openevse/command"
+	"github.com/avegao/openevse/util"
+)
+
+type getOverTemperatureThresholdsInterface interface {
+	Run(response string) (firmwareVersion string, protocolVersion string, err error)
+	parseResponse(response string) (firmwareVersion string, protocolVersion string, err error)
+}
+
+type getOverTemperatureThresholds struct {
+	getOverTemperatureThresholdsInterface
+	command.Command
+}
+
+func (c getOverTemperatureThresholds) Run(host string) (ambient float32, ir float32, err error) {
+	c.Type = command.GetOverTemperatureThresholds
+
+	response, err := c.SendRequest(host)
+
+	if err != nil {
+		return
+	}
+
+	return c.parseResponse(response.Response)
+}
+
+func (c getOverTemperatureThresholds) parseResponse(response string) (ambient float32, ir float32, err error) {
+	split := strings.Split(response, " ")
+
+	switch split[0] {
+	case command.SuccessResponse:
+		var ambientInt, irInt int
+
+		if ambientInt, err = util.ParseInt(split[1]); err != nil {
+			return
+		}
+
+		if irInt, err = util.ParseInt(split[2]); err != nil {
+			return
+		}
+
+		ambient = float32(ambientInt) / float32(10)
+		ir = float32(irInt) / float32(10)
+	case command.FailureResponse:
+		err = errors.New("openevse - invalid request")
+	default:
+		err = errors.New("openevse - invalid request - unknown error")
+	}
+
+	return
+}
+
+func New() getOverTemperatureThresholds {
+	c := new(getOverTemperatureThresholds)
+	c.Type = command.GetOverTemperatureThresholds
+
+	return *c
+}
---
+++ command/get/over_temperature_thresholds/get_over_temperature_thresholds_test.go
@@ -0,0 +1,40 @@
+package getOverTemperatureThresholds
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/h2non/gock.v1"
+	"fmt"
+	"net/http"
+	"github.com/avegao/openevse/command"
+)
+
+func TestGetOverTemperatureThresholds_Run(t *testing.T) {
+	const (
+		expectedAmbient float32 = 89.5
+		expectedIr float32 = 56.5
+	)
+
+	defer gock.Disable()
+
+	r := gock.New("http://(.*)").
+		Reply(http.StatusOK).
+		JSON(
+		command.Response{
+			Command:  fmt.Sprintf("$%s", command.GetOverTemperatureThresholds),
+			Response: fmt.Sprint("$OK 895 565"),
+		})
+
+	assert.NoError(t, r.Error)
+
+	ambient, ir, err := New().Run("192.168.1.156")
+
+	assert.NoError(t, err)
+	assert.Equal(t, expectedAmbient, ambient)
+	assert.Equal(t, expectedIr, ir)
+	assert.True(t, gock.IsDone())
+}
+
+func TestNew(t *testing.T) {
+	assert.NotEmpty(t, New())
+}
