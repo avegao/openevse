9c77aa1caf613c72d1de0c20545f3f369bd74eda
Álvaro de la Vega Olmedilla <alvarodlvo@gmail.com>
2018-01-29 19:51:45 +0100

    * ADD: Get settings command

---
+++ command/get/settings/get_settings.go
@@ -0,0 +1,58 @@
+package getSettings
+
+import (
+	"strings"
+	"errors"
+	"github.com/avegao/openevse/command"
+	"github.com/avegao/openevse/util"
+)
+
+type getSettingsCommandInterface interface {
+	Run(response string) (amperes int, flags interface{}, err error)
+	parseResponse(response string) (amperes int, flags interface{}, err error)
+}
+
+type getSettingsCommand struct {
+	getSettingsCommandInterface
+	command.Command
+}
+
+func (c getSettingsCommand) Run(host string) (amperes int, flags interface{}, err error) {
+	c.Type = command.GetSettings
+
+	response, err := c.SendRequest(host)
+
+	if err != nil {
+		return
+	}
+
+	return c.parseResponse(response.Response)
+}
+
+func (c getSettingsCommand) parseResponse(response string) (amperes int, flags int, err error) {
+	split := strings.Split(response, " ")
+
+	switch split[0] {
+	case command.SuccessResponse:
+		if amperes, err = util.ParseInt(split[1]); err != nil {
+			return
+		}
+
+		if flags, err = util.ParseHexInt(split[2]); err != nil {
+			return
+		}
+	case command.FailureResponse:
+		err = errors.New("invalid request")
+	default:
+		err = errors.New("invalid request - unknown error")
+	}
+
+	return
+}
+
+func New() getSettingsCommand {
+	c := new(getSettingsCommand)
+	c.Type = command.GetSettings
+
+	return *c
+}
---
+++ command/get/settings/get_settings_test.go
@@ -0,0 +1,40 @@
+package getSettings
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/h2non/gock.v1"
+	"fmt"
+	"github.com/avegao/openevse/command"
+	"net/http"
+)
+
+func TestGetSettingsCommand_Run(t *testing.T) {
+	const (
+		expectedAmperes = 16
+		expectedFlags   = 33
+	)
+
+	defer gock.Disable()
+
+	r := gock.New("http://(.*)").
+		Reply(http.StatusOK).
+		JSON(
+		command.Response{
+			Command:  fmt.Sprintf("$%s", command.GetSettings),
+			Response: "$OK 16 0021^16",
+		})
+
+	assert.NoError(t, r.Error)
+
+	amperes, flags, err := New().Run("192.168.1.156")
+
+	assert.NoError(t, err)
+	assert.Equal(t, expectedAmperes, amperes)
+	assert.Equal(t, expectedFlags, flags)
+	assert.True(t, gock.IsDone())
+}
+
+func TestNew(t *testing.T) {
+	assert.NotEmpty(t, New())
+}
